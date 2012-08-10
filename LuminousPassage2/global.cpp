921273be7e06961e45e1a334dac4cdf85c34128e
Daniel Santos <dsantos@alum.mit.edu>
2012-08-10 08:51:54 -0700

    Latest changes.

---
+++ LuminousPassage2/global.cpp
@@ -0,0 +1,14 @@
+// File: global.cpp
+
+//*****************
+// INCLUDES
+//*****************
+
+#include "global.h"
+
+//*****************
+// GLOBAL VARIABLES
+//*****************
+
+volatile unsigned char gComMasterSlaveBytes[GLOBAL_NUM_CHANNELS];
+
